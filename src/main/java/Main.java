98fe41b5314496c934350cd0a329adfa948d9d8b
Mathieu Duchesneau <mathieu@doubledash.io>
2024-02-23 09:29:29 +0000

    fix reading sequence in Main.java + fix package def in build.gradle, Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,15 @@ import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 
 public class Main {
-  public static void main(String[] args) {
-    // replace with appropriate path, interval, and baudRate
-    String path = "/dev/ttyACM0";
-    int interval = 1000;
-    int baudRate = 9600;
+  // replace with appropriate path, interval, and baudRate
+  static String path = "/dev/ttyACM0";
+  static int interval = 1000;
+  static int baudRate = 9600;
 
+  static String info_line;
+  static int padlen;
+
+  public static void main(String[] args) {
     // open port
     SerialPort serialPort = new SerialPort(path);
     try {
@@ -38,67 +41,24 @@ public class Main {
     // read event
     try {
       serialPort.addEventListener(new SerialPortEventListener() {
-        String info_line;
-        int padlen;
+        StringBuilder msg = new StringBuilder();
+
 
         @Override
         public void serialEvent(SerialPortEvent event) {
           if (event.isRXCHAR()) {
             try {
               byte[] buffer = serialPort.readBytes();
-              if (buffer != null) {
-                String data = new String(buffer);
-                String[] split = data.replace(", ", ",").split(",");
+              for (byte b: buffer) {
+                if ( (b == '\r' || b == '\n') && msg.length() > 0) {
+                  String data = new String(msg.toString());
 
-                // extract info line
-                if (split[0].equals("I")) {
-                  // parse field titles
-                  if (split[1].equals("Product ID")) {
-                    info_line = data;
-                    String[] infoSplit = info_line.split(",");
-                    String[] values = new String[infoSplit.length - 4];
-                    for (int i = 4; i < infoSplit.length; i++) {
-                      values[i - 4] = infoSplit[i];
-                    }
-                    double[] numberValues = new double[values.length / 2];
-                    for (int i = 0; i < values.length; i += 2) {
-                      numberValues[i / 2] = Double.parseDouble(values[i]);
-                    }
-                    padlen = 0;
-                    for (String s : values) {
-                      if (s.length() > padlen) {
-                        padlen = s.length();
-                      }
-                    }
-                    System.out.println(info_line);
-                  } else {
-                    System.out.println(split[3]);
-                  }
-                  return;
+                  processData(data);
+                  msg.setLength(0);
                 }
-                if (info_line == null) {
-                  System.out.println("Awaiting info line...");
-                  return;
+                else {
+                  msg.append((char)b);
                 }
-
-                // extract readout values
-                String device = split[1] + " " + split[2];
-                String[] sensors = info_line.split(",");
-                double[] values = new double[sensors.length / 2];
-                String[] units = new String[sensors.length / 2];
-                for (int i = 4; i < sensors.length; i += 2) {
-                  sensors[i] = sensors[i].trim();
-                  values[i / 2] = Double.parseDouble(split[i]);
-                  units[i / 2] = split[i + 1].trim();
-                }
-
-                // print result
-                String now = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
-                System.out.println(now + " " + device);
-                for (int i = 0; i < units.length; i++) {
-                  System.out.println(String.format("%-" + (padlen + 2) + "s %s %s", sensors[i], values[i], units[i]));
-                }
-                System.out.println("\n");
               }
             } catch (SerialPortException ex) {
               System.out.println("Error reading from serial port: " + ex);
@@ -109,5 +69,57 @@ public class Main {
     } catch (SerialPortException ex) {
       System.out.println("Error setting up event listener: " + ex);
     }
+  }
+
+  private static void processData(String data) {
+    //String data = new String(buffer);
+    String[] split = data.replace(", ", ",").split("\\*")[0].split(",");
+
+    // extract info line
+    if (split[0].contains("I")) {
+      // parse field titles
+      if (split[1].equals("Product ID")) {
+        info_line = data;
+        String[] infoSplit = info_line.split(",");
+        String[] values = new String[infoSplit.length - 4];
+        for (int i = 4; i < infoSplit.length; i++) {
+          values[i - 4] = infoSplit[i];
+        }
+
+        padlen = 0;
+        for (String s : values) {
+          if (s.length() > padlen) {
+            padlen = s.length();
+          }
+        }
+        System.out.println(info_line);
+      } else {
+        System.out.println(split[3]);
+      }
+      return;
+    }
+    if (info_line == null) {
+      System.out.println("Awaiting info line...");
+      return;
+    }
+
+    // extract readout values
+    String device = split[1] + " " + split[2];
+    String[] sensors = info_line.split(",");
+    double[] values = new double[sensors.length / 2];
+    String[] units = new String[sensors.length / 2];
+
+    for (int i = 4; i < sensors.length - 1; i += 2) {
+      values[i / 2] = Double.parseDouble(split[i]);
+      units[i / 2] = split[i + 1].trim();
+    }
+
+    // print result
+    String now = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+    System.out.println(now + " " + device);
+    for (int i = 0; i < units.length; i++) {
+      System.out.println(String.format("%-" + (padlen + 2) + "s %s %s", sensors[i], values[i], units[i]));
+    }
+    System.out.println("\n");
   }
 }
